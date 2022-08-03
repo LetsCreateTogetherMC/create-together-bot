4c910a575b4b69d99e05209ac2d3e1d26d244d4e
Nemesis <shuklaakshatascom@gmail.com>
2022-08-03 17:34:40 +0530

    Added more commands and requirements.txt

--- db.py
+++ db.py
@@ -4,6 +4,11 @@ import mysql.connector
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("OS_PASSWORD")
 
+# @todo
+# 1) Add Show Leaderboards - DONE
+# 2) Add delete <leaderboard>
+# 3) Add Pagination to show <leaderboard>
+
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -36,10 +41,18 @@ def create_db(db_name: str) -> None:
     cursor.execute(f"USE {db_name}")
     db.commit()
 
+    create_global_table()
+
     print("Database Created!")
 
 
-def create_table(name: str) -> dict:
+def create_global_table() -> None:
+    cursor.execute(
+        f"CREATE TABLE global(leaderboard VARCHAR(100) NOT NULL, symbol VARCHAR(50) DEFAULT ':coin:')")
+    db.commit()
+
+
+def create_table(name: str, symbol: str = ":coin:") -> dict:
     cursor.execute("SHOW TABLES")
     tables = cursor.fetchall()
 
@@ -52,6 +65,9 @@ def create_table(name: str) -> dict:
 
     cursor.execute(
         f"CREATE TABLE {name}(user_id BIGINT PRIMARY KEY, user_name VARCHAR(50) NOT NULL, points INT DEFAULT 0)")
+    cursor.execute(
+        f"INSERT INTO global VALUES('{name}', '{symbol}')"
+    )
     db.commit()
     print(f"Created Table {name}")
     return {
@@ -91,25 +107,89 @@ def add_points_to_user(leaderboard: str, user_id: int, amount: int, user_name: s
     if not user:
         print(f"User not found in {leaderboard}")
         add_user_to_table(leaderboard, user_id, user_name)
-        # return {
-        #     "success": False,
-        #     "message": f"User not found in {leaderboard}"
-        # }
 
     cursor.execute(f"UPDATE {leaderboard} SET points = points + {amount}")
     db.commit()
-    print(f"Added {amount} Points to {user_id}")
+
+    symbol = get_symbol(leaderboard)
+    print(f"Added {amount} {symbol} to {user_id}")
 
     return {
         "success": True,
-        "message": None
+        "message": None,
+        "symbol": symbol
     }
 
 
 def get_leaderboard(leaderboard: str) -> dict:
     cursor.execute(f"SELECT * FROM {leaderboard} ORDER BY points")
     data = cursor.fetchall()
+    symbol = get_symbol(leaderboard)
     return {
         "success": True,
-        "message": data
+        "message": data,
+        "symbol": symbol
     }
+
+
+def get_leaderboards() -> dict:
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+
+    return {
+        "success": True,
+        "message": tables
+    }
+
+
+def delete_leaderboard(leaderboard: str) -> dict:
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    found = False
+
+    for table in tables:
+        if table[0] == leaderboard.lower():
+            found = True
+            break
+
+    if not found:
+        return {
+            "success": False,
+            "message": f"Leaderboard {leaderboard} not found!"
+        }
+
+    cursor.execute(f"DROP TABLE {leaderboard}")
+    cursor.execute(
+        f"DELETE FROM global WHERE leaderboard LIKE '{leaderboard}'")
+    db.commit()
+    return {
+        "success": True,
+        "message": f"Deleted Leaderboard {leaderboard}"
+    }
+
+
+def clear_db() -> dict:
+    """Deletes all the tables from the database"""
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+
+    for table in tables:
+        table_name = table[0]
+        cursor.execute(f"DROP TABLE {table_name}")
+    db.commit()
+
+    create_global_table()
+
+    return {
+        "success": True,
+        "message": "Deleted all the leaderboards"
+    }
+
+# Utils
+
+
+def get_symbol(leaderboard: str) -> str:
+    cursor.execute(
+        f"SELECT symbol from global WHERE leaderboard LIKE '{leaderboard}'")
+    symbol = cursor.fetchone()[0]
+    return symbol
--- main.py
+++ main.py
@@ -20,12 +20,16 @@ async def on_ready():
     print(f"Logged in as {bot.user}")
     db.init_db(DB_NAME)
 
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the cogwheels"))
+
 
 @bot.command(name="create", help="Create a new Leaderboard")
-async def create_leaderboard(ctx, leaderboard):
+async def create_leaderboard(ctx, leaderboard, symbol=":coin:"):
     if verify_role(ctx):
-        res = db.create_table(leaderboard)
-        await ctx.send(res["message"])
+        res = db.create_table(leaderboard, symbol)
+
+        embed = generate_embed(res["success"], res["message"])
+        await ctx.send(embed=embed)
 
 
 @bot.command(name="add", help="Add Points to a User in a Leaderboard")
@@ -35,11 +39,14 @@ async def add_points(ctx, leaderboard, user: discord.Member, amount: int):
             await ctx.send("Amount cannot be 0")
             return
 
-        print(user.id)
-        res = db.add_points_to_user(leaderboard, user.id, amount, user.name)
+        user_name: str = f"{user.display_name}#{user.discriminator}"
+        res = db.add_points_to_user(
+            leaderboard, user.id, amount, user_name)
 
-        msg = res["message"] if res["message"] else f"Added {amount} points to {user.name}"
-        await ctx.send(msg)
+        msg = res["message"] if res["message"] else f"Added {amount} {res['symbol']} to {user_name}"
+
+        embed = generate_embed(res["success"], msg)
+        await ctx.send(embed=embed)
 
 
 @bot.command(name="remove", help="Remove Points from a User in a Leaderboard")
@@ -49,18 +56,41 @@ async def remove_points(ctx, leaderboard, user: discord.Member, amount: int):
             await ctx.send("Amount cannot be 0")
             return
 
-        res = db.add_points_to_user(leaderboard, user.id, -amount, user.name)
+        res = db.add_points_to_user(
+            leaderboard, user.id, -amount, user.display_name)
 
-        msg = res["message"] if res["message"] else f"Removed {amount} points from {user.name}"
-        await ctx.send(msg)
+        msg = res["message"] if res["message"] else f"Removed {amount} {res['symbol']} from {user.display_name}"
+
+        embed = generate_embed(res["success"], msg)
+        await ctx.send(embed=embed)
 
 
 @bot.command(name="show", help="View a Leaderboard")
-async def show_leaderboard(ctx, leaderboard):
+async def show_leaderboard(ctx, leaderboard=""):
     if verify_role(ctx):
-        res = db.get_leaderboard(leaderboard)
-        msg = format_table(leaderboard, res["message"])
-        await ctx.send(msg)
+        if not leaderboard:
+            res = db.get_leaderboards()
+            msg = format_leaderboard_list(res["message"])
+            await ctx.send(embed=msg)
+        else:
+            res = db.get_leaderboard(leaderboard)
+            msg = format_rankings(leaderboard, res["message"], res["symbol"])
+            await ctx.send(embed=msg)
+
+
+@bot.command(name="clear", help="Removes all the leaderboards")
+async def clear(ctx, leaderboard=""):
+    if verify_role(ctx):
+        if not leaderboard:
+            res = db.clear_db()
+
+            msg = generate_embed(res["success"], res["message"])
+            await ctx.send(embed=msg)
+        else:
+            res = db.delete_leaderboard(leaderboard)
+
+            msg = generate_embed(res["success"], res["message"])
+            await ctx.send(embed=msg)
 
 # Utility Functions
 
@@ -70,14 +100,61 @@ def verify_role(ctx) -> bool:
     return required_role in ctx.author.roles
 
 
-def format_table(leaderboard: str, data: list) -> str:
-    msg = f"> **{leaderboard.capitalize()}**\n> \n"
+def format_rankings(leaderboard: str, data: list, symbol: str) -> discord.Embed:
+    embed = discord.Embed(
+        colour=0x0084ff,
+        title=f"{leaderboard.capitalize()} Rankings"
+    )
 
     for idx in range(len(data)):
         item = data[idx]
-        msg += f"> {idx+1} - {item[1]} : {item[2]}\n"
 
-    return msg
+        embed.add_field(
+            name=f"{idx + 1} - {item[1]}",
+            value=f"{symbol} {item[2]}"
+        )
+
+    if len(data) == 0:
+        embed.add_field(
+            name="",
+            value="*Its lonely here...*"
+        )
+
+    return embed
+
+
+def format_leaderboard_list(tables: list) -> discord.Embed:
+    embed = discord.Embed(
+        title="Leaderboards",
+        colour=0x0084ff
+    )
+
+    idx = 1
+
+    for table in tables:
+        table_name = table[0]
+
+        if table_name == "global":
+            continue
+
+        symbol = db.get_symbol(table_name)
+        embed.add_field(
+            name=f"{idx} - {table_name.capitalize()}",
+            value=symbol,
+            inline=False
+        )
+        idx += 1
+
+    return embed
+
+
+def generate_embed(success: bool, message: str) -> discord.Embed:
+    embed = discord.Embed(
+        colour=0x0084ff if success else 0x991b1b,
+        description=message,
+    )
+
+    return embed
 
 
 bot.run(DISCORD_TOKEN)
